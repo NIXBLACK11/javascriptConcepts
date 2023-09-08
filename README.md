12f3495aafa926501a9e7d7d7053a9ef113bd646
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-08 20:45:18 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # javascriptConcepts
+
+
+JavaScript is inherently single-threaded and non-blocking, which means it has the appearance of being synchronous (one operation executing at a time) even when dealing with asynchronous operations. This can be somewhat confusing, so let's break it down:
+
+- Single-Threaded: 
+    JavaScript runs in a single execution thread within a web browser or a Node.js environment. This means that at any given moment, only one operation is executed, and the next operation has to wait until the current one finishes.
+
+- Non-Blocking: 
+    JavaScript uses an event loop to manage asynchronous operations. When an asynchronous operation (e.g., making an HTTP request, reading a file, or setting a timer) is initiated, JavaScript doesn't block the entire program's execution. Instead, it registers a callback function to be executed when the operation is complete. This allows other code to continue running while waiting for the asynchronous operation to finish.
--- asyncAwaitPromisesCallbacks/promises.js
+++ asyncAwaitPromisesCallbacks/promises.js
@@ -1,3 +1,7 @@
+// The best way to deal with promises is async await
+
+
+
 const posts = [
     { title: 'Post One', body: 'This is post one'},
     { title: 'Post Two', body: 'This is post two'}
@@ -34,4 +38,38 @@ function createPost(post) {
 
 
 //apply .catch for if there is an error
-createPost({ title: 'Post Three', body: 'This is post three'}).then(getPosts).catch(err => console.log(err));
+// createPost({ title: 'Post Three', body: 'This is post three'}).then(getPosts).catch(err => console.log(err));
+
+//Async // Await  a more elegant way to handle promises
+// async function init() {
+//     await createPost({ title: 'Post Three', body: 'This is post three'});
+
+//     getPosts();
+// }
+
+// init();
+
+//  async and  await wiht fetch
+async function fetchUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    console.log(data);
+}
+
+fetchUsers();
+
+
+// if there are a lot of promises you have to use a lot of dot then
+// the time taken to return is the time for the max promise
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, 'Goodbye');
+// });
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json());
+
+
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+
