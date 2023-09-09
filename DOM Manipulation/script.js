1db6101ac9f37ee855b8fc470b5136434a7b0028
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-09 13:46:36 +0530

    dom

---
+++ DOM Manipulation/script.js
@@ -0,0 +1,19 @@
+// const body = document.body;
+// body.append("Hello", "Bye");
+// body.appendChild("<h1>Hello</h1>");
+
+// const div = document.createElement('div');
+
+// div.innerText = "Hello World 1";
+// // or
+// div.textContent = "Hello World 2";
+
+// div.innerHTML = "<h1>headere</h1>";
+
+// body.append(div);
+
+
+const div = document.querySelector('div');
+
+console.log(div.textContent);
+console.log(div.innerHTML);
